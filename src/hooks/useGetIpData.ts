a4b786bffb984f926fce0424fc09e8e36108f293
Mohd Azeem Malik (Ubuntu) <azeemkhandsari@gmail.com>
2024-09-27 20:47:28 +0530

    API changed

--- src/hooks/useGetIpData.ts
+++ src/hooks/useGetIpData.ts
@@ -6,7 +6,6 @@ export const useGetIpData = async (ip: string)=>{
     try {
         const response = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&domain=${ip}`)
         const data = response.data
-        console.log(data)
         return data
     } catch (error) {
         console.log(error as Error)
